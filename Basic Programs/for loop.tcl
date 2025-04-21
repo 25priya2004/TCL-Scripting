971fe3494f246bbd524ec7d0a798193eee0ea9c7
Priyadharshini Murugesan <146159293+25priya2004@users.noreply.github.com>
2025-04-21 23:44:13 +0530

    Create for loop.tcl

---
+++ Basic Programs/for loop.tcl
@@ -0,0 +1,11 @@
+% for {set i 0} {$i < 10} {incr i} {puts "The number is [expr {$i + 1}]"}    
+The number is 1
+The number is 2
+The number is 3
+The number is 4
+The number is 5
+The number is 6
+The number is 7
+The number is 8
+The number is 9
+The number is 10
